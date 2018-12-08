6c7dbbb70bd54807843f6d68caa9ecf46032cfb2
Idan Nikritin <idan.nik@gmail.com>
2018-12-08 17:04:13 +0200

    header

--- ChessSite/urls.py
+++ ChessSite/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from chess import views as chess_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', chess_views.home_page)
 ]
---
+++ functional_tests/test_page.py
@@ -0,0 +1,11 @@
+import time
+
+from functional_tests.base import FunctionalTest
+
+
+class PageTest(FunctionalTest):
+    def test_a(self):
+        self.browser.get(self.live_server_url)
+        text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('Chess', text)
+
