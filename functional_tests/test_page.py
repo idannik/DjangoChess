d1475535810394c5042c6edfd409efff90c1632c
Idan Nikritin <idan.nik@gmail.com>
2018-12-20 12:58:17 +0200

    add learn the basics list item

--- functional_tests/test_page.py
+++ functional_tests/test_page.py
@@ -4,8 +4,14 @@ from functional_tests.base import FunctionalTest
 
 
 class PageTest(FunctionalTest):
-    def test_a(self):
+    def test_title(self):
         self.browser.get(self.live_server_url)
         text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('Chess', text)
+
+        list = self.browser.find_element_by_tag_name('ul')
+        rows = list.find_elements_by_tag_name('li')
+        self.assertIn('Learn the basics!', [row.text for row in rows])
+
+        print(repr(list))
 
